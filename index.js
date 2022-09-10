313f28a205de70aee81ce90bf98b3645c4a431ac
Nego <61694826+kardespro@users.noreply.github.com>
2022-09-11 02:12:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const axios = require("axios")
+const BASE_URL = "https://packages.nego.network/packages/negoai/release.json"
+const config = {
+   "errorMessage": "Nego API Error | packages.nego.network not resolving"
 
+}
+module.exports = async (content, options = {}) => {
+    let package_req = await axios.get(BASE_URL)
+    let resolve = package_req.data
+    if(!content) return new Error("Question not Provided")
+    if(!options) return new Error("Options Not Provided")
+
+   // let data = resolve.find(a => a.question === content && a.isCursedResponse === options.isCursedResponse)
+   let data = resolve.find(a => a.question.includes(content) && a.isCursedResponse === options.isCursedResponse)
+
+    return data.answer
+}
